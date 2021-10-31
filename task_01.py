aad2008c1513b0c20890f380af838170408302d3
Nikita Smirnov <nvsmirnov00@yandex.ru>
2021-10-31 14:22:31 +0300

    Error correction

--- task_01.py
+++ task_01.py
@@ -1,18 +1,14 @@
 def is_palindrome(sentence):
-    if not isinstance(sentence, str):
-        return False
-    else:
-        sentence = sentence.lower()
-        start_of_line, end_of_line = 0, len(sentence) - 1
-        while start_of_line < end_of_line:
-            if not sentence[start_of_line].isalpha():
-                start_of_line += 1
-            elif not sentence[end_of_line].isalpha():
-                end_of_line -= 1
-            elif sentence[start_of_line] != sentence[end_of_line]:
-                return False
-            else:
-                start_of_line += 1
-                end_of_line -= 1
-        return True
-
+    sentence = str(sentence).lower()
+    start_of_line, end_of_line = 0, len(sentence) - 1
+    while start_of_line < end_of_line:
+        if not sentence[start_of_line].isalpha():
+            start_of_line += 1
+        elif not sentence[end_of_line].isalpha():
+            end_of_line -= 1
+        elif sentence[start_of_line] != sentence[end_of_line]:
+            return False
+        else:
+            start_of_line += 1
+            end_of_line -= 1
+    return True
--- task_05.py
+++ task_05.py
@@ -4,7 +4,8 @@ import datetime
 def date_in_future(days):
     if not isinstance(days, int):
         current_date = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-        print(current_date)
+        return current_date
     else:
         future_date = (datetime.datetime.today() + datetime.timedelta(days=days)).strftime('%d-%m-%Y %H:%M:%S')
-        print(future_date)
+        return future_date
+
